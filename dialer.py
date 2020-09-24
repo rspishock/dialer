a6af9aaa5f545c9a48db9be08fb20e635628754f
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-09-23 20:10:12 -0400

    Add multiple features to script.

--- dialer.py
+++ dialer.py
@@ -1,33 +1,45 @@
+#!/usr/local/bin/python3
+
 from random import randint
-from time import sleep
+from tqdm import tqdm
 from sys import exit
 import argparse
 import pyttsx3
+import time
 
 
 def create_number():
     temp = ''
     number = []
     count = 1
-    print('Generating number')
+    # print('Generating number')
     while count <= 10:
-        number.append(str(randint(0,9)))
-        count+=1
+        number.append(str(randint(0, 9)))
+        count += 1
 
     final = temp.join(number)
 
     return str(final)
 
 
-message = 'Hello Senator, it\'s time for you to do the job that you were elected for.'
+def progress_bar():
+    # defaults to 30 seconds, change 3 to 10 for 5 minute wait
+    for i in tqdm(range(10)):
+        time.sleep(3)
+
+
+message = 'Hello Senator.  For too long, you\'ve held a position of great importance in this country, living off of ' \
+          'the tax payers dollar but failed to perform your duties.  It\'s time for you to do the job that you were ' \
+          'elected for.'
+total_calls = 1
 
 while True:
     try:
         # generate 'from' number
         test = create_number()
-        final_number = (f'({test[0:3]}) {test[3:6]}-{test[6:]}')
-        print(f'Number: {final_number}')
-        sleep(5)
+        call_from = f'({test[0:3]}) {test[3:6]}-{test[6:]}'
+        print(f'Call from: {call_from}')
+        time.sleep(5)
 
         # play message
         engine = pyttsx3.init()
@@ -36,10 +48,12 @@ while True:
 
         # wait 5 minutes for next call
         print('Sleep')
-        sleep(5)  # change to 300 for 5 minutes
+        progress_bar()
+        # sleep(5)  # change to 300 for 5 minutes
         print()
+        total_calls += 1
+
     except KeyboardInterrupt:
         print('Quitting script.')
+        print(f'Total calls made: {str(total_calls)}')
         exit()
-
-
