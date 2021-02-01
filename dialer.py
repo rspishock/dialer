d05017bc081218fb364271013f027f3529428414
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2021-01-31 19:42:19 -0500

    Added arguement function to script.  User can specify target and number of times to dial.

--- dialer.py
+++ dialer.py
@@ -8,6 +8,18 @@ import pyttsx3
 import time
 
 
+def get_arguments():
+    """Get user supplied arguments from terminal."""
+    parser = argparse.ArgumentParser()
+    # arguments
+    parser.add_argument('-c', '--count', dest='count', help='Specify the number of times you want the dialer to call.')
+    parser.add_argument('-t', '--target', dest='target', help='Specify the target number for the script to call.')
+
+    (options) = parser.parse_args()
+
+    return options
+
+
 def create_number():
     temp = ''
     number = []
@@ -28,30 +40,40 @@ def progress_bar():
         time.sleep(3)
 
 
-message = 'Hello you are being called by a Python script.'
+message = 'Hello, you are being called by a Python script.'
 total_calls = 1
 
+options = get_arguments()
+count = options.count     # number of times dialer will call
+target = options.target   # the target number for the dialer to call
+
 while True:
-    try:
-        # generate 'from' number
-        test = create_number()
-        call_from = f'({test[0:3]}) {test[3:6]}-{test[6:]}'
-        print(f'Call from: {call_from}')
-        time.sleep(5)
+    if total_calls < int(count):
+        try:
+            # generate 'from' number
+            test = create_number()
+            call_from = f'({test[0:3]}) {test[3:6]}-{test[6:]}'
+            print(f'Call from: {call_from}')
+            time.sleep(5)
 
-        # play message
-        engine = pyttsx3.init()
-        engine.say(message)
-        engine.runAndWait()
+            print(f'Dialing: {target}')
 
-        # wait 5 minutes for next call
-        print('Sleep')
-        progress_bar()
-        # sleep(5)  # change to 300 for 5 minutes
-        print()
-        total_calls += 1
+            # play message
+            engine = pyttsx3.init()
+            engine.say(message)
+            engine.runAndWait()
 
-    except KeyboardInterrupt:
-        print('Quitting script.')
-        print(f'Total calls made: {str(total_calls)}')
-        exit()
+            # wait 5 minutes for next call
+            print('Sleep')
+            progress_bar()
+            # sleep(5)  # change to 300 for 5 minutes
+            print()
+            total_calls += 1
+
+        except KeyboardInterrupt:
+            print('Quitting script.')
+            print(f'Total calls made: {str(total_calls)}')
+            exit()
+    else:
+        print(f'You have dialed {target} a total of {count} times.')
+        break
